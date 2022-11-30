01282b42baf9245ac6fbdf826e57952343a0fb3c
edr99 <ederbittencourt@rocketmail.com>
2022-11-29 18:39:49 -0800

    updated schemes but still have to complete security section again

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,3 +1,27 @@
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
 const Joi = require("joi");
 
 module.exports.campgroundSchema = Joi.object({
