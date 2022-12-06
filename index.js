f7dd421764395f9d9510b6d34e42ff30fc730726
edr99 <ederbittencourt@rocketmail.com>
2022-12-05 20:58:05 -0800

    port changes

--- index.js
+++ index.js
@@ -140,6 +140,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("listening");
 });
